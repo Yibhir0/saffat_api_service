406a2c0daa93f4a95fce65b8b7acaadd9bffd6e0
Yibhir0 <65478935+Yibhir0@users.noreply.github.com>
2024-05-11 16:11:41 -0400

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -1,6 +1,7 @@
 CREATE TABLE prayer (
     prayer_id INTEGER PRIMARY KEY,
     prayer_name TEXT NOT NULL,
+    prayer_name_arabic TEXT NOT NULL,
     adan_time TEXT NOT NULL,
     iqama_time TEXT NOT NULL,
     notified_at TEXT NOT NULL
